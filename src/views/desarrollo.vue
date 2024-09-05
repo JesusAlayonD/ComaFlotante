4e5ae80ce7cb8f7582824ccaa684b4aba523153c
JesusAlayonD <jesusalayond@gmail.com>
2024-09-04 22:06:05 -0600

    Corrigiendo desarrollo

--- src/views/desarrollo.vue
+++ src/views/desarrollo.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
             <form name="property_form">
                 <h1>Universidad Juárez Autónoma de Tabasco</h1>
                 <h2>Jesús Alayón Domínguez <br>Kevin Alí Maldonado López<br><br>Números de punto flotante<br><br>Métodos
-                    númericos<br>Docente: Prof. Héctor Javier Toraya Lazo de la vega<br>Grupo:</h2>
+                    númericos<br>Docente: Prof. Héctor Javier Toraya Lazo de la vega<br>Grupo: C17</h2>
                 <br>
                 <RouterLink to="/" class="custom-button">Regresar</RouterLink>
             </form>
